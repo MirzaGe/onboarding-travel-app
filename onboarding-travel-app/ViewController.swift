2e97e6156bf1246eb22e4e98d58674d107e7ab05
Sherry1481 <shareyscorpi1@gmail.com>
2021-05-18 17:23:01 +0500

    app complete almost

--- onboarding-travel-app/ViewController.swift
+++ onboarding-travel-app/ViewController.swift
@@ -18,16 +18,16 @@ class ViewController: UIViewController {
     private let items: [OnboardingItem] = [
         .init(title: "Travel Your Way",
               detail: "Travel the world by air, rail or sea at the most competitive prices",
-              bgImage: nil),
+              bgImage: UIImage(named: "imTravel1")),
         .init(title: "Stay Your Way",
               detail: "With Over Millions Of Hotels worldwide, find the perfect accomodation in the most amazing places!",
-              bgImage: nil),
+              bgImage: UIImage(named: "imTravel2")),
         .init(title: "Discover Your Way With New Features",
               detail: "Explore exotic destinations with our new features that link you to like-minded travellers!",
-              bgImage: nil),
+              bgImage: UIImage(named: "imTravel3")),
         .init(title: "Feast Your Way",
               detail: "We recommend you local cuisines that are safe and highly recommended by the locals!",
-              bgImage: nil)
+              bgImage: UIImage(named: "imTravel4"))
         
     ]
     
@@ -37,8 +37,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setupPageControl()
         setupScreen(index: currentPage)
+        updateBackgroundImage(index: currentPage)
         setupGestures()
+        setupViews()
+        
+        
     }
+    
+    private func setupViews() {
+        darkView.backgroundColor = UIColor.init(white: 0.1, alpha: 0.5)
+    }
+    
+    private func updateBackgroundImage(index: Int) {
+        let image = items[index].bgImage
+        UIView.transition(with: bgImageView,
+                          duration: 0.5, options: .transitionCrossDissolve,
+                          animations:{ self.bgImageView.image = image },
+                          completion: nil)
+        
+    }
+    
     
     private func setupPageControl() {
         pageControl.numberOfPages = items.count
@@ -59,6 +77,7 @@ class ViewController: UIViewController {
     }
     
     @objc private func handleTapAnimation() {
+        // 1st animation = detail label
         
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations : {
             self.titleLabel.alpha = 0.8
@@ -70,12 +89,19 @@ class ViewController: UIViewController {
             }, completion: nil)
         }
         
+     // second animation detail label
         
         UIView.animate(withDuration: 0.5, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations:
                         {
                             self.detailLabel.alpha = 0.8
                             self.detailLabel.transform = CGAffineTransform(translationX: -30, y: 0)
                         }) { _ in
+            
+            
+            if self.currentPage < self.items.count - 1 {
+                self.updateBackgroundImage(index: self.currentPage + 1)
+            }
+            
             
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
                 self.detailLabel.alpha = 0
